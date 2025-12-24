a3550fa669fc99928bceb48b22e65c7c89bfbc91
MFakhryzaKi <mfakhry.zaki@gmail.com>
2025-12-24 22:31:29 +0700

    release

--- src/NMF.py
+++ src/NMF.py
@@ -1,42 +1,46 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 # SUBPROCESS
 
-def readCSVtoMat (review, product):
-    pd_review = pd.read_csv(review)
-    pd_product = pd.read_csv(product)
+folder_src = Path(__file__).parent
+def readCSVtoMat ():
+    path_review = folder_src.parent / "data" / "reviews.csv"
+    path_product = folder_src.parent / "data" / "products.csv"
+    
+    pd_review = pd.read_csv(path_review)
+    pd_product = pd.read_csv(path_product)
 
     product_category = pd_product['category'].values
 
+    review_reviewID = pd_review['review_id'].values
     review_userID = pd_review['user_id'].values
     review_product = pd_review['product_id'].values
     review_rating = pd_review['rating'].values
 
     # ubah product_id jadi category
     for i in range(len(review_product)):
-        temp = int(review_product[1:])
-        review_product[i] = product_category[temp]
+        temp = review_product[i][1:]
+        temp = int(temp)
+        review_product[i] = product_category[temp-1]
 
-    resMatrix = []
-    cols = []
-    rows = []
+    rows = np.unique(review_userID)
+    cols = np.unique(review_product)
+    resMatrix = np.zeros((len(rows), len(cols)))
+
+    user_to_idx = {user: i for i, user in enumerate(rows)}
+    prod_to_idx = {prod: j for j, prod in enumerate(cols)}
+    
     # bikin matriks dari ratingnya
-    for i in range(len(review_userID)):
-        if (review_userID[i] not in rows):
-            rows = np.append((rows, review_userID[i]))
-            baris_nol = np.zeros_like(resMatrix.shape[0])
-            resMatrix = np.vstack((resMatrix, baris_nol))
-        if (review_product[i] not in cols) :
-            cols = np.append((cols, review_product[i]))
-            kolom_nol = np.zeros_like(resMatrix.shape[1])
-            resMatrix = np.hstack((resMatrix, kolom_nol))
-        rowId = np.where(rows == review_userID[i])[0][0]
-        colId = np.where(cols == review_product[i])[0][0]
+    for i in range(len(review_reviewID)) :
+        rowId = user_to_idx[review_userID[i]]
+        colId = prod_to_idx[review_product[i]]
+
         if resMatrix[rowId][colId] == 0 :
             resMatrix[rowId][colId] = review_rating[i]
 
-    return resMatrix
+    return resMatrix, cols
 
 
 def printMat (matriks) :
@@ -80,12 +84,14 @@ def frobeniusNorm (matriks) :
     return res
 
 def getUserInput (cols):
-    userInput = np.zeros((1, cols))
-    validInput = np.zeros((1, cols), dtype=bool)
-    for i in range(cols):
+    lenCols = len(cols)
+    userInput = np.zeros((1, lenCols))
+    validInput = np.zeros((1, lenCols), dtype=bool)
+    for i in range(lenCols):
         while (True) :
             try:
-                valStr = input(f"Masukkan rating terhadap item {i} (masukkan s atau skip untuk melewati)\n")
+                valStr = input(f"Masukkan rating (0 - 5) terhadap item "
+                               f"{cols[i]} (masukkan s atau skip untuk melewati)\n")
 
                 if (valStr == "s" or valStr == "skip") :
                     validInput[0][i] = False
@@ -97,24 +103,24 @@ def getUserInput (cols):
                     validInput[0][i] = True
                     break
                 else:
-                    print("Masukan harus berada di rentang 0-5")
+                    print("Masukan harus berada di rentang 0 - 5")
 
             except ValueError:
-                print("Masukan harus berupa angka dalam rentang 0-5")
+                print("Masukan harus berupa angka dalam rentang 0 - 5")
         
         if (validInput[0][i]) :
             userInput[0][i] = valFloat
         
     return userInput, validInput
 
-def printSortedRecommendation(validInput, resMatrix, k) :
+def printSortedRecommendation(validInput, resMatrix, listItem, k) :
     recomendationList = []
     r,c = resMatrix.shape
 
     for i in range (c):
         if (not validInput[0][i]) :
             recomendationList.append ({
-                "id": i,
+                "name": listItem[i],
                 "val": resMatrix[r-1][i]
             })
     
@@ -124,56 +130,36 @@ def printSortedRecommendation(validInput, resMatrix, k) :
         reverse=True
     )
 
-    print("rekomendasi item untuk user: ")
-    for i in range (len(recomendationList)) :
+    print("\nRekomendasi Item Untuk User: ")
+    for i in range (min(k, len(recomendationList))) :
         item = recomendationList[i]
-        print(f"{i+1}. {item['id']} -> {item['val']:.4f}")
-
-    # return recomendationList[:k]
+        print(f"{i+1}. {item['name']} : ({item['val']:.4f} / 5.0)")
+    print()
 
 # MAIN
 
-demoMatAwal = np.array([
-    [5, 3, 0, 1],
-    [4, 0, 0, 1],
-    [1, 1, 0, 5],
-    [0, 0, 5, 4]
-])
+V, listItem = readCSVtoMat()
+row, col = V.shape
 
-row, col = demoMatAwal.shape
+userInput, validInput = getUserInput(listItem)
+V = np.append(V, userInput, axis=0)
 
-userInput, validInput = getUserInput(col)
-demoMatAwal = np.append(demoMatAwal, userInput, axis=0)
+row, col = V.shape
 
-row, col = demoMatAwal.shape
 iter = 100
-k = 3
+k = 5
+toleransi = 1e-9
 
 W = np.random.rand(row, k)
 H = np.random.rand(k, col)
 
-# print("matriks awal adalah :")
-# printMat(demoMatAwal)
-# print("mat W awal :")
-# printMat(W)
-# print("mat H awal :")
-# printMat(H)
-
 for i in range(iter) :
-    W = transformW(H, W, demoMatAwal) # W_baru
-    H = transformH(H, W, demoMatAwal) # H_baru
+    W = transformW(H, W, V) # W_baru
+    H = transformH(H, W, V) # H_baru
 
-    if (frobeniusNorm(demoMatAwal - W @ H) < 1e-9) :
+    if (frobeniusNorm(V - W @ H) < toleransi) :
         break
 
-# print("hasil matriks adalah : ")
 resMatrix = W @ H
-# printMat(resMatrix)
 
-# print(f"frobenius normnya = {frobeniusNorm(demoMatAwal - W @ H)}")
-
-printSortedRecommendation(validInput, resMatrix, 5)
-# print("rekomendasi item untuk user: ")
-# for i in range (len(recomendationList)) :
-#     item = recomendationList[i]
-#     print(f"{i+1}. {item['id']} -> {item['val']:.4f}")
+printSortedRecommendation(validInput, resMatrix, listItem, 5)
